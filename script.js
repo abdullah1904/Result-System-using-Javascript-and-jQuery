cf61ee867b471db81569a7218c2078637e375082
Muhammad Abdulllah <mabdullahzahid782@gmail.com>
2023-04-16 07:44:51 +0500

    Updated

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ var dfname = document.getElementById("fname");
 var drollno = document.getElementById("rollno");
 var dredgno = document.getElementById("redgno");
 var databox = document.querySelector('.data');
-var resultbox = document.querySelector('.result')
+var resultbox = document.querySelector('.result');
+var DPF = document.getElementById("dpf");
+var DPFL = document.getElementById("dpfl");
+var DITC = document.getElementById("ditc");
+var DITCL = document.getElementById("ditcl");
+var DCALCULUS = document.getElementById("dcal");
+var DFE = document.getElementById("dfe");
+var DAPHY = document.getElementById("daphy");
+var DAPHYL = document.getElementById("daphyl");
+var DWS = document.getElementById("dws");
 // Class for students data entry
 class student{
     constructor(Name="Not Enter",rollnumber="Not Enter",fathername="Not Enter",regdnumber="Not Enter"){
@@ -29,27 +38,27 @@ class student{
     }
 }
 // Objects for students data entry
-var students = [];
-students[0] = new student("Reshail Kareem", 500, "Abdul Kareem" , "2022 CS-500");
-students[0].entry("A+","A+","A+","A+","A+","A+","A+","A+","A+");
-students[1] = new student("Muhmmad Abdullah", 525, "Zahid Mehmood" ,"2022 CS-525");
-students[1].entry("A","A-","A","A","C-","A-","B-","B+","B-");
+var students =[];
+students[0] = new student("Muhmmad Abdullah", 525, "Zahid Mehmood" ,"2022 CS-525")
+students[0].entry("A","A-","A","A","C-","A-","B-","B+","B-");
 // Main Function
 $(document).ready(function(){
     $(button).click(function(){
         var checkflag = false;
+        $(databox).fadeOut("fast");
+        $(error).fadeOut("fast");
+        $(resultbox).fadeOut("fast");
         for(let i = 0; i<students.length;i++){
             if(students[i].rollno == input.value){
                 $(databox).fadeIn("slow");
                 databox.style.display = "flex";
-                $(error).fadeOut("slow");
+                $(resultbox).fadeIn("slow");
                 display(i);
                 checkflag = true;
                 break;
             }
         }
         if(checkflag == false){
-            $(databox).fadeOut("slow");
             $(error).fadeIn("slow");
         }
     });
@@ -60,4 +69,13 @@ var display = (x)=>{
     dfname.innerHTML = students[x].fname;
     drollno.innerHTML = students[x].rollno;
     dredgno.innerHTML = students[x].redgno;
+    DPF.innerHTML = students[x].pf;
+    DPFL.innerHTML = students[x].pfl;
+    DITC.innerHTML = students[x].itc;
+    DITCL.innerHTML = students[x].itcl;
+    DFE.innerHTML = students[x].fe;
+    DCALCULUS.innerHTML = students[x].calculus;
+    DAPHY.innerHTML = students[x].aphy;
+    DAPHYL.innerHTML = students[x].aphyl;
+    DWS.innerHTML = students[x].workshop;
 }
